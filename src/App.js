eb656fb024768aa82413e0a35df884736be2cc20
alouimouma <mohamedaloui401@gmail.com>
2021-06-03 12:29:25 -1200

    ajout checkpoint

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserList from "./UserList/index";
+function App() {
+    return (
+        <div className="App">
+            <h1>List of users</h1>
+            <UserList />
+        </div>
+    );
+}
+
+export default App;
